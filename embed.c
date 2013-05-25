d841d2aac1adafd84234d36824a4d52342c94eac
Matias De Santi <mdesanti90@gmail.com>
2013-05-25 14:46:44 -0300

    Added embed.c

---
+++ embed.c
@@ -0,0 +1,17 @@
+//
+//  embed.c
+//  TP2-Cripto
+//
+//  Created by Matias De Santi on 25/05/13.
+//  Copyright (c) 2013 Matias De Santi. All rights reserved.
+//
+
+#include <stdio.h>
+#include "image.h"
+#include <openssl/aes.h>
+#include <openssl/des.h>
+
+image* embedLSB1(image* in, char* toHide, image* hideHere) {
+    
+    
+}
